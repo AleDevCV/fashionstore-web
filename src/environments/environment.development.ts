a404de6ebf2c6e4059add6e5d40bd89cc89e8001
AleDevCV <acovi2014@yahoo.com>
2026-09-24 23:33:02 -0400

    feat: migrar probador AR a Snap Camera Kit SDK

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -16,4 +16,11 @@ export const environment = {
 
   /** Clave con la que se guarda el token JWT en el localStorage del navegador. */
   claveTokenJwt: 'fashionstore_token',
+
+  snapCameraKit: {
+    appId: 'fe230af1-b5cd-48b6-83b0-8c3f664b71b4',
+    apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzkwMzA1MjI0LCJzdWIiOiJmZTIzMGVmMS1iNWNkLTQ4YjYtODNiMC04YzNmNjY0YjcxYjR-U1RBR0lOR35kY2Y2ODIzNi1jNjhkLTRlY2YtYjU2Ny0wYmU4YThlM2Q5ZjkifQ.UYAnPqqmMutR1mie9_QrQBuAhMOvofzMfxdfjaKWy4c',
+    lensGroupId: '', // LO NECESITAMOS
+    testLensId: '05287a80-d455-4317-807b-314a336d7a59'
+  }
 };
