5c6dd1afd843318e5dc4c61b5002cff8cd58b740
AleDevCV <acovi2014@yahoo.com>
2026-09-24 23:41:57 -0400

    fix: update Lens Group ID for Camera Kit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -16,7 +16,7 @@ export const environment = {
   snapCameraKit: {
     appId: 'fe230af1-b5cd-48b6-83b0-8c3f664b71b4',
     apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzkwMzA1MjI0LCJzdWIiOiJmZTIzMGVmMS1iNWNkLTQ4YjYtODNiMC04YzNmNjY0YjcxYjR-U1RBR0lOR35kY2Y2ODIzNi1jNjhkLTRlY2YtYjU2Ny0wYmU4YThlM2Q5ZjkifQ.UYAnPqqmMutR1mie9_QrQBuAhMOvofzMfxdfjaKWy4c',
-    lensGroupId: '', 
+    lensGroupId: 'f2b09303-fd4f-45d2-a95c-22e07d74d831', 
     testLensId: '05287a80-d455-4317-807b-314a336d7a59' 
   }
 };
